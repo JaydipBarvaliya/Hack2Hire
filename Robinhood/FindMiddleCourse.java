ae59362ef7acef6f693dae657ef0d7e110983c59
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-09-25 00:05:35 -0400

    Add StockTradesMatching and update FindMiddleCourse logic
    Introduced StockTradesMatching.java with exactTradeMatch method for matching trades. Refactored FindMiddleCourse.java to find the middle course in the longest prerequisite path using DFS and memoization, replacing the previous linear approach.

--- Robinhood/FindMiddleCourse.java
+++ Robinhood/FindMiddleCourse.java
@@ -1,26 +1,101 @@
+// import java.util.*;
+
+// class Solution {
+//     public String findMiddleCourse(List<List<String>> pairs) {
+
+//         Map<String, String> map = new HashMap<>();
+//         Set<String> setOfCourses = new HashSet<>();
+
+//         for (List<String> pair : pairs) {
+//             map.put(pair.get(0), pair.get(1));
+//             setOfCourses.add(pair.get(0));
+//             setOfCourses.add(pair.get(1));
+//         }
+
+//         int totalCourses = setOfCourses.size();
+//         map.entrySet().forEach(entry -> setOfCourses.remove(entry.getValue()));
+//         String firstCourse = setOfCourses.iterator().next();
+
+//         int mid = totalCourses / 2;
+//         for (int i = 0; i < mid; i++) {
+//             firstCourse = map.get(firstCourse);
+//         }
+//         return firstCourse;
+
+//     }
+// }
+
+
+
+
 import java.util.*;
 
 class Solution {
-    public String findMiddleCourse(List<List<String>> pairs) {
+    public String findLongestPathMiddleCourse(List<List<String>> pairs) {
 
-        Map<String, String> map = new HashMap<>();
-        Set<String> setOfCourses = new HashSet<>();
+        Map<String, List<String>> map = new HashMap<>();
+        Set<String> courses = new HashSet<>();
 
         for (List<String> pair : pairs) {
-            map.put(pair.get(0), pair.get(1));
-            setOfCourses.add(pair.get(0));
-            setOfCourses.add(pair.get(1));
+
+            String key = pair.get(0);
+            String value = pair.get(1);
+            
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>(Arrays.asList(value)));
+            }else{
+                map.get(key).add(value);
+            }
+
+            courses.add(pair.get(0));
+            courses.add(pair.get(1));
         }
 
-        int totalCourses = setOfCourses.size();
-        map.entrySet().forEach(entry -> setOfCourses.remove(entry.getValue()));
-        String firstCourse = setOfCourses.iterator().next();
+        map.values().forEach(list -> list.forEach( val -> courses.remove(val)) );
 
-        int mid = totalCourses / 2;
-        for (int i = 0; i < mid; i++) {
-            firstCourse = map.get(firstCourse);
+
+        List<String> longestPath = new ArrayList<>();
+        Map<String, List<String>> memo = new HashMap<>();
+        for(String sourceNode : courses){ 
+            List<String> path  = dfs(sourceNode, map, new ArrayList<>(), memo);
+
+            if(path.size() > longestPath.size()){
+                longestPath = path;
+            }
         }
-        return firstCourse;
+        
+        if(longestPath.size() % 2 == 0){
+            int mid = (longestPath.size()/2)-1;
+            return longestPath.get(mid);
+        }else{
+            int mid = longestPath.size()/2;
+            return longestPath.get(mid);
+        }
 
+    }
+
+    public List<String> dfs(String node, Map<String, List<String>> map, List<String> chain, Map<String, List<String>> memo){
+        
+        if(memo.containsKey(node)) return memo.get(node);
+
+        chain.add(node);
+
+        if(!map.containsKey(node)) return chain;
+    
+        List<String> longest = new ArrayList<>();
+
+        for(String neigh : map.get(node)){
+            
+            List<String> newPath = dfs(neigh, map, new ArrayList<>(chain), memo);
+
+            if(newPath.size() > longest.size()){
+                longest = newPath;
+            }
+        }
+
+        longest.add(0, node); // add current node at the beginning of the longest path
+
+        memo.put(node, longest);
+        return longest;
     }
 }
