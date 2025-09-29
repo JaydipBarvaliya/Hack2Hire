4c313cafd9b1c4fee9cd29907b1d1fcc17f58f1a
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-09-29 13:21:33 -0400

    Update StockTradesMatching I.java

--- Robinhood/StockTradesMatching I.java
+++ Robinhood/StockTradesMatching I.java
@@ -1,33 +1,36 @@
 import java.util.*;
 
+
+// TreeMap operations are O(log n) instead of O(1) for HashMap.
+// If your dataset is huge and sorting at the end was cheap compared to insertions, HashMap + Collections.sort() can sometimes still be faster.
 class Solution {
     public List<String> exactTradeMatch(List<String> houses, List<String> streets) {
 
         Map<String, Integer> houseMap = new HashMap<>();
 
-        for (String house : houses) {
-            houseMap.put(house, houseMap.getOrDefault(house, 0) + 1);
+        for(String house: houses){
+            houseMap.put(house, houseMap.getOrDefault(house, 0) + 1 );
         }
 
         List<String> result = new ArrayList<>();
 
-        for (String street : streets) {
-            if (houseMap.containsKey(street)) {
+        for(String street: streets){
+            if(houseMap.containsKey(street)){
 
                 int freq = houseMap.get(street) - 1;
-                if (freq == 0) {
+                if(freq == 0){
                     houseMap.remove(street);
-                } else {
+                }else{
                     houseMap.put(street, freq);
                 }
-            } else {
+            }else{
                 result.add(street);
             }
         }
 
         houseMap.entrySet().forEach(entry -> {
 
-            for (int i = 0; i < entry.getValue(); i++) {
+            for(int i=0; i<entry.getValue(); i++){
                 result.add(entry.getKey());
             }
         });
@@ -36,9 +39,5 @@ class Solution {
 
         return result;
 
-    }
+     }
 }
-
-
- house=["AAPL,B,0100,ABC123", "AAPL,B,0100,ABC123", "AAPL,B,0100,ABC123", "GOOG,S,0050,CDC333", "AAPL,S,0100,ABC456"],
-street=["  FB,B,0100,GBGGGG", "AAPL,B,0100,ABC123", "AAPL,B,0100,ABC123", "GOOG,S,0050,CDC444"]
